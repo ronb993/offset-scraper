de755e08c7551395de0806ccba1b1f831e4a6260
verlooy <verlooy@gmail.com>
2022-05-28 15:59:57 -0500

    added discord table for offsets

--- scraper.py
+++ scraper.py
@@ -3,102 +3,68 @@ import requests, re, dHook
 from bs4 import BeautifulSoup
 from table2ascii import table2ascii as t2a, PresetStyle
 
+URLS={ 'CBaseEntity' : 'https://apex.dumps.host/?class=CBaseEntity',
+    'cPlayer' : 'https://apex.dumps.host/?class=CPlayer',
+    'offsets' : 'https://apex.dumps.host/offsets'
+}
+
 def sendDiscord(msg, url: str):
     if not url:
         return
     data = {"content": f"```\n{msg}\n```"}
     requests.post(url, json=data)
 
-# Current Offset values
-levelName = '0x135cd80'
-clEntityList = '0x19fbcd8'
-localPlayer = '0x1daca88'
-localOrigin = '0x58'
-iTeamNum = '0x448'
-iName = '0x589'
-lifeState = '0x798'
-viewAngles = '0x2588'
-bleedoutState = '0x2728'
-
-URLS={
-    'CBaseEntity' : 'https://apex.dumps.host/?class=CBaseEntity',
-    'cPlayer' : 'https://apex.dumps.host/?class=CPlayer',
-    'offsets' : 'https://apex.dumps.host/offsets'
-}
-
-cbaseOffsets={
-    'localOrigin' : 'm_localOrigin', # must add (0x100)
-    'iTeamNum' : 'm_iTeamNum',
-    'iName' : 'm_iName'
-}
-
-cplayerOffsets={
-    'lifeState' : 'm_lifeState',
-    'viewAngles' : 'm_ammoPoolCapacity', # subtract (0x14)
-    'bleedoutState' : 'm_bleedoutState'
-}
-
-coreOffsets={
-    'levelName' : 'level_name',
-    'clEntityList' : 'cl_entitylist',
-    'localPlayer' : 'local_player'
-}
-
-page = requests.get(URLS['CBaseEntity'])
-soup = BeautifulSoup(page.content, "html.parser")
-results = soup.find_all('tbody', class_='bg-gray-50 dark:bg-gray-700 divide-y divide-gray-200 dark:divide-gray-500')
-
-def get_cbaseOffsets():
-    #todo - create for loop for dict values
-    offset_one = cbaseOffsets['localOrigin']
-    offset_two = cbaseOffsets['iTeamNum']
-    offset_three = cbaseOffsets['iName']
-    result_list = get_results()
-    for result in result_list:
-        if result == offset_one:
-            offset = result_list.index(offset_one) + 2
-            newOffset = result_list[offset]
-            if newOffset == localOrigin:
-                print(cbaseOffsets['localOrigin'], "offset has not changed")
-            else:
-                print(cbaseOffsets['localOrigin'], "offset has changed")
-        elif result == offset_two:
-            offset = result_list.index(offset_two) + 2
-            newOffset = result_list[offset]
-            if newOffset == iTeamNum:
-                print(cbaseOffsets['iTeamNum'], "offset has not changed")
-            else:
-                print(cbaseOffsets['iTeamNum'], "offset has changed")
-
-        elif result == offset_three:
-            offset = result_list.index(offset_three) + 2
-            newOffset = result_list[offset]
-            if newOffset == iName:
-                print(cbaseOffsets['iName'], "offset has not changed")
-            else:
-                print(cbaseOffsets['iName'], "offset has changed")
-
-
-# Todo - add functions for cplayer and coreoffsets, then return all values to a dictionary
-#def get_cplayerOffsets():
-#def get_coreOffsets():
-
-
-
 
 def get_results():
+    page = requests.get(URLS['CBaseEntity'])
+    soup = BeautifulSoup(page.content, "html.parser")
+    results = soup.find_all('tbody', class_='bg-gray-50 dark:bg-gray-700 divide-y divide-gray-200 dark:divide-gray-500')
     for result in results:
         list = re.sub(r"(\n)+", r"\n", re.sub(r"{3, }", "", result.get_text())).split()
     return list
 
-get_cbaseOffsets()
+def get_cbaseOffsets():
+    cbaseOffsets = {}
+    result_list = get_results()
+    for result in result_list:
+        if result == 'm_localOrigin':
+            offsetIndex = result_list.index('m_localOrigin') + 2
+            offsetValue = result_list[offsetIndex]
+            cbaseOffsets['m_localOrigin'] = offsetValue
 
+        elif result == 'm_iTeamNum':
+            offsetIndex = result_list.index('m_iTeamNum') + 2
+            offsetValue = result_list[offsetIndex]
+            cbaseOffsets['m_iTeamNum'] = offsetValue
+            
+        elif result == 'm_iName':
+            offsetIndex = result_list.index('m_iName') + 2
+            offsetValue = result_list[offsetIndex]
+            cbaseOffsets['m_iName'] = offsetValue
 
-myResults = t2a( # todo add dictionary values here to reflect the table
-    header=["Offset", "Value", "Old Value", "Change?"],
-    body=[['m_localOrigin', 0, 0, 'No'], ['m_iTeamNum', 0, 0, 'No'], ['m_iName', 0, 0, 'No']],
-    style=PresetStyle.thin_compact
-)
+    return cbaseOffsets
 
-sendDiscord(myResults, dHook.url) #todo fix table, it looks funky when window isnt maximized
+def checkResults():
+    x = get_cbaseOffsets()
+    if x['m_localOrigin'] == '0x58':
+        change_one = 'no'
+    else:
+        change_one = 'yes'
+    if x['m_iTeamNum'] == '0x448':
+       change_two = 'no'
+    else:
+        change_two = 'yes'
+    if x['m_iName'] == '0x589':
+        change_three = 'no'
+    else:
+        change_three = 'yes'
+    myResults = t2a( # todo add dictionary values here to reflect the table
+        header=["Offset", "Value", "Change?"],
+        body=[["localOrigin", x['m_localOrigin'], change_one], ["iTeamNum", x['m_iTeamNum'], change_two], ["iName", x['m_iName'], change_three]],
+        column_widths=[13] * 3,
+        style=PresetStyle.ascii_box 
+    )
+    return myResults
 
+checkResults()
+sendDiscord(checkResults(), dHook.url) #todo fix table, it looks funky when window isnt maximized
